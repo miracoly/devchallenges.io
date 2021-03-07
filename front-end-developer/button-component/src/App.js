43524143bee02836418d6aeeb99a1099f4666532
miracoly <68049792+miracoly@users.noreply.github.com>
2021-03-07 20:35:32 +0100

    create production build

--- front-end-developer/button-component/src/App.js
+++ front-end-developer/button-component/src/App.js
@@ -1,5 +1,11 @@
 import "./App.css";
+import Button from "./components/button/Button";
 
-const App = () => <h1>Button Component</h1>;
+const App = () => (
+  <>
+    <h1>Button Component</h1>
+    <Button />
+  </>
+);
 
 export default App;
---
+++ front-end-developer/button-component/src/components/button/Button.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./button.css";
+
+const Button = (props) => <a href="">Button</a>;
+
+export default Button;
