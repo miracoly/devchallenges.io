c7055ba915ab717276ac03ccc93fe678845084a5
miracoly <68049792+miracoly@users.noreply.github.com>
2021-03-13 23:12:21 +0100

    adding color and icon prop

--- front-end-developer/button-component/src/App.js
+++ front-end-developer/button-component/src/App.js
@@ -2,43 +2,82 @@ import "./App.css";
 import Button from "./components/button/Button";
 
 const App = () => (
-  <div className="container">
+  <main>
     <h1>Button Component</h1>
-    <div className="button-container">
-      <p>{"<Button />"}</p> <Button>Default</Button>
+    <div className="container">
+      <div className="button-container">
+        <p>{"<Button />"}</p> <Button>Default</Button>
+      </div>
+      <div className="button-container">
+        <p>{"<Button variant='outline' />"}</p>
+        <Button variant="outline">Default</Button>
+      </div>
+      <div className="button-container">
+        <p>{"<Button variant='text' />"}</p>
+        <Button variant="text">Default</Button>
+      </div>
+      <div className="button-container">
+        <p>{"<Button disableShadow />"}</p>
+        <Button disableShadow>Default</Button>
+      </div>
+      <div className="button-container">
+        <p>{"<Button disabled />"}</p>
+        <Button disabled>Disabled</Button>
+      </div>
+      <div className="button-container">
+        <p>{"<Button variant='text'  disabled />"}</p>
+        <Button variant="text" disabled>
+          Disabled
+        </Button>
+      </div>
     </div>
-    <div className="button-container">
-      <p>{"<Button variant='outline' />"}</p>
-      <Button variant="outline">Default</Button>
+
+    <div className="container">
+      <div className="button-container">
+        <p>{"<Button size='sm' />"}</p> <Button size="sm">Default</Button>
+      </div>
+      <div className="button-container">
+        <p>{"<Button size='md' />"}</p> <Button size="md">Default</Button>
+      </div>
+      <div className="button-container">
+        <p>{"<Button size='lg' />"}</p> <Button size="lg">Default</Button>
+      </div>
     </div>
-    <div className="button-container">
-      <p>{"<Button variant='text' />"}</p>
-      <Button variant="text">Default</Button>
+
+    <div className="container">
+      <div className="button-container">
+        <p>{"<Button color='default' />"}</p>
+        <Button color="default">Default</Button>
+      </div>
+      <div className="button-container">
+        <p>{"<Button color='primary' />"}</p>
+        <Button color="primary">Primary</Button>
+      </div>
+      <div className="button-container">
+        <p>{"<Button color='secondary' />"}</p>
+        <Button color="secondary">Secondary</Button>
+      </div>
+      <div className="button-container">
+        <p>{"<Button color='danger' />"}</p>
+        <Button color="danger">Danger</Button>
+      </div>
     </div>
-    <div className="button-container">
-      <p>{"<Button disableShadow />"}</p>
-      <Button disableShadow>Default</Button>
+
+    <div className="container">
+      <div className="button-container">
+        <p>{"<Button startIcon='local_grocery_store' />"}</p>
+        <Button color="primary" startIcon="local_grocery_store">
+          Default
+        </Button>
+      </div>
+      <div className="button-container">
+        <p>{"<Button endIcon='local_grocery_store' />"}</p>
+        <Button color="primary" endIcon="local_grocery_store">
+          Default
+        </Button>
+      </div>
     </div>
-    <div className="button-container">
-      <p>{"<Button disabled />"}</p>
-      <Button disabled>Disabled</Button>
-    </div>
-    <div className="button-container">
-      <p>{"<Button variant='text'  disabled />"}</p>
-      <Button variant="text" disabled>
-        Disabled
-      </Button>
-    </div>
-    <div className="button-container">
-      <p>{"<Button size='sm' />"}</p> <Button size="sm">Default</Button>
-    </div>
-    <div className="button-container">
-      <p>{"<Button size='md' />"}</p> <Button size="md">Default</Button>
-    </div>
-    <div className="button-container">
-      <p>{"<Button size='lg' />"}</p> <Button size="lg">Default</Button>
-    </div>
-  </div>
+  </main>
 );
 
 export default App;
--- front-end-developer/button-component/src/components/button/Button.jsx
+++ front-end-developer/button-component/src/components/button/Button.jsx
@@ -2,11 +2,33 @@ import React from "react";
 
 import "./button.css";
 
-const Button = ({ variant, size, disableShadow, disabled, children }) => {
+const Button = (props) => {
+  const {
+    variant,
+    size,
+    color,
+    disableShadow,
+    disabled,
+    startIcon,
+    endIcon,
+    children,
+  } = props;
+
   // add classes to from props to classList
   let classList = ["button"];
   classList.push(disableShadow ? "disable-shadow" : undefined);
   classList.push(variant);
+  classList.push(color);
+  if (startIcon || endIcon) {
+    classList.push("icon");
+  }
+  if (startIcon) {
+    classList.push("start-icon");
+    classList.push(startIcon);
+  } else if (endIcon) {
+    classList.push("end-icon");
+    classList.push(endIcon);
+  }
   classList.push(size ? `size-${size}` : undefined);
   classList.push(disabled ? "disabled" : undefined);
   classList = classList.filter((className) => className);
