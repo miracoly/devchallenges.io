db77da9b803a166e9edc17a52e46d5d8c6edb2ce
miracoly <68049792+miracoly@users.noreply.github.com>
2021-03-13 18:03:20 +0100

    removing button-container component

--- front-end-developer/button-component/src/App.js
+++ front-end-developer/button-component/src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
-import ButtonContainer from "./components/button-container/ButtonContainer";
+import Button from "./components/button/Button";
 
 const App = () => (
   <div className="container">
     <h1>Button Component</h1>
-    <ButtonContainer desc="<Button />" />
-    <ButtonContainer desc="<Button variant=”outline” />" variant="outline" />
-    <ButtonContainer desc="<Button variant=”text” />" variant="text" />
+    <div className="button-container">
+      <p>{"<Button />"}</p> <Button>Default</Button>
+    </div>
+    <div className="button-container">
+      <p>{"<Button variant=”outline” />"}</p>
+      <Button variant="outline">Default</Button>
+    </div>
+    <div className="button-container">
+      <p>{"<Button variant=”text” />"}</p>
+      <Button variant="text">Default</Button>
+    </div>
   </div>
 );
 
