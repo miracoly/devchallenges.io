0d01abbd2774ec929853ddb3e86cb316984e1637
miracoly <68049792+miracoly@users.noreply.github.com>
2021-03-28 21:01:35 +0200

    add container for overall look

--- front-end-developer/input-component/src/App.js
+++ front-end-developer/input-component/src/App.js
@@ -3,31 +3,34 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Input>{"<Input />"}</Input>
-      <Input error>{"<Input error />"}</Input>
-      <Input disabled>{"<Input disabled />"}</Input>
-      <Input helperText="Some interesting text">
-        {"<Input helperText='Some interesting text' />"}
-      </Input>
-      <Input error helperText="Some interesting text">
-        {"<Input error helperText='Some interesting text' />"}
-      </Input>
-      <Input startIcon="face">{"<Input startIcon='face' />"}</Input>
-      <Input error startIcon="add_task">
-        {"<Input error startIcon='add_task' />"}
-      </Input>
-      <Input endIcon="circle_notifications">
-        {"<Input endIcon='circle_notifications' />"}
-      </Input>
-      <Input value="Text">{"<Input value='Text' />"}</Input>
-      <Input size="sm">{"<Input size='sm' />"}</Input>
-      <Input size="md">{"<Input size='md' />"}</Input>
+    <main>
+      <div className="container">
+        <Input>{"<Input />"}</Input>
+        <Input error>{"<Input error />"}</Input>
+        <Input disabled>{"<Input disabled />"}</Input>
+        <Input helperText="Some interesting text">
+          {"<Input helperText='Some interesting text' />"}
+        </Input>
+
+        <Input error helperText="Some interesting text">
+          {"<Input error helperText='Some interesting text' />"}
+        </Input>
+        <Input startIcon="face">{"<Input startIcon='face' />"}</Input>
+        <Input error startIcon="add_task">
+          {"<Input error startIcon='add_task' />"}
+        </Input>
+        <Input endIcon="circle_notifications">
+          {"<Input endIcon='circle_notifications' />"}
+        </Input>
+        <Input value="Text">{"<Input value='Text' />"}</Input>
+        <Input size="sm">{"<Input size='sm' />"}</Input>
+        <Input size="md">{"<Input size='md' />"}</Input>
+      </div>
       <Input fullWidth>{"<Input fullWidth />"}</Input>
       <Input multiLine row="4">
         {"<Input multiLine row='4' />"}
       </Input>
-    </div>
+    </main>
   );
 }
 
