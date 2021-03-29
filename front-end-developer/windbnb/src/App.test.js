807e53181099dbea9744cf572d804b1f19ceeb63
miracoly <68049792+miracoly@users.noreply.github.com>
2021-03-29 20:14:21 +0200

    add failing test for displaying header component

--- front-end-developer/windbnb/src/App.test.js
+++ front-end-developer/windbnb/src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("Display Header component", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerComponent = screen.getByTestId("header-component");
+  expect(headerComponent).toBeInTheDocument();
 });
